92ec2e7f3af4b20ffe4172eff66f1ae22318dd51
sanskar-23 <kapoorshanskar@gmail.com>
2024-01-06 20:46:11 +0530

    Added Footer Section

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -13,29 +13,29 @@ const About = () => {
           <div className="about__cards">
             <article className="about__card">
               <MdPeopleAlt className="about__icon" />
-              <h5> Experinece </h5>{" "}
+              <h5> Experience </h5>
               <small> SDE Summer Intern at Barclays India. </small> <br />
-              <small> June - July 2023 </small>{" "}
-            </article>{" "}
+              <small> June - July 2023 </small>
+            </article>
             <article className="about__card">
               <GiBrain className="about__icon" />
-              <h5> Coding Problems Solved </h5>{" "}
-              <small> 1000 + Across Various Platforms </small>{" "}
-            </article>{" "}
+              <h5> Coding Problems Solved </h5>
+              <small> 1000 + Across Various Platforms </small>
+            </article>
             <article className="about__card">
               <GiGraduateCap className="about__icon" />
-              <h5> Education </h5>{" "}
+              <h5> Education </h5>
               <small> CSE at TIET Patiala, expected 2024 </small>{" "}
-            </article>{" "}
-          </div>{" "}
+            </article>
+          </div>
           <p className="para">
-            Hey There!I am Sanskar Kapoor, former SDE Intern at Barclays India
+            Hey There! I am Sanskar Kapoor, former SDE Intern at Barclays India
             and an Upcoming Software Engineer at Barclays India, final year CSE
-            Student at Thapar Institute of Engineering and Technology, Patiala.I
-            like coding and problem - solving, I have Solved over 1000 +
-            Problems on various platforms like Leetcode and GFG.I am comfortable
-            with Full - Stack web development and have worked with libraries and
-            frameworks like React, Express and Node.{" "}
+            Student at Thapar Institute of Engineering and Technology, Patiala.
+            I like coding and problem-solving, I have Solved over 1000+ Problems
+            on various platforms like Leetcode and GFG. I am comfortable with
+            Full-Stack web development and have worked with libraries and
+            frameworks like React, Express and Node.
           </p>{" "}
         </div>{" "}
       </div>{" "}
--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
-  return <section id="footer"> Footer </section>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Sanskar Kapoor
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/sanskar-kapoor-9a3264214/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
