b815bee9571875c01b6f7ccf4d6e8f7e5902b409
sanskar-23 <kapoorshanskar@gmail.com>
2024-01-06 16:46:42 +0530

    Added EmailJS Feature

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -17,7 +17,6 @@ const App = () => {
       <Skills />
       <Services />
       <Portfolio />
-
       <Contact />
       <Footer />
     </>
--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,8 +1,90 @@
 import React from "react";
 import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsLinkedin } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_klyb8no",
+      "template_weuxbxm",
+      form.current,
+      "_jqYxSFsoQ8dDh-Ig"
+    );
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>kapoorshanskar@gmail.com</h5>
+            <a
+              href="mailto:kapoorshanskar@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send an Email
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+            <h4>LinkedIn</h4>
+            <h5>Sanskar Kapoor</h5>
+            <a
+              href="https://www.linkedin.com/in/sanskar-kapoor-9a3264214/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Connect on LinkedIn
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+91 9837099166</h5>
+            <a
+              href="https://wa.me/919837099166"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
